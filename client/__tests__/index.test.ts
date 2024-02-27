fcc60f153270ed49954db32ef7b8811c501c0abb
Femtech-web <bossfemzy10@gmail.com>
2024-02-27 13:46:17 +0100

    feat: setup husky git hook

---
+++ client/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import '@testing-library/jest-dom';
+
+describe("Homepage", () => {
+  it("renders a homepage", () => {
+    console.log("it worked!")
+  })
+})
